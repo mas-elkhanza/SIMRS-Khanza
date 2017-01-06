f03aed25b2a7805ac1cef203f08a13a490938be5
khanzasoft <khanzasoft@khanzasoft>
2017-01-06 17:17:27 +0700

    lisensi.txt/README.TXT bro.Penambahan form komponen darah

--- src/setting/DlgUser.java
+++ src/setting/DlgUser.java
@@ -86,7 +86,7 @@ public class DlgUser extends javax.swing.JDialog {
                     "[J]Jurnal Harian","[D]Sirkulasi Obat, Alkes & BHP 2","[A]Edit Registrasi","[K]Referensi Diagnosa","[K]Referensi Poli","[D]Industri Farmasi",
                     "[H]Harian J.S.","[H]Bulanan J.S.","[H]Harian BHP Medis/Paket Obat","[H]Bulanan BHP Medis/Paket Obat","[J]Piutang Belum Lunas","[K]Referensi Faskes",
                     "[K]Data Bridging SEP","[D]Pengambilan BHP UTD","[J]Tarif UTD","[M]Pengambilan BHP Medis","[M]BHP Medis Rusak","[E]Pengambilan UTD","[M]Pengambilan BHP Non Medis",
-                    "[M]BHP Non Medis Rusak","[E]Suplier Non Medis","[M]Donor Darah","[K]Monitoring Verifikasi Klaim","[M]Cekal Darah"
+                    "[M]BHP Non Medis Rusak","[E]Suplier Non Medis","[M]Donor Darah","[K]Monitoring Verifikasi Klaim","[M]Cekal Darah","[M]Komponen Darah"
         };
         
         tabMode=new DefaultTableModel(null,row){
@@ -151,7 +151,7 @@ public class DlgUser extends javax.swing.JDialog {
                 java.lang.Boolean.class, java.lang.Boolean.class, java.lang.Boolean.class, java.lang.Boolean.class, 
                 java.lang.Boolean.class, java.lang.Boolean.class, java.lang.Boolean.class, java.lang.Boolean.class, 
                 java.lang.Boolean.class, java.lang.Boolean.class, java.lang.Boolean.class, java.lang.Boolean.class, 
-                java.lang.Boolean.class
+                java.lang.Boolean.class, java.lang.Boolean.class
              };
              @Override
              public Class getColumnClass(int columnIndex) {
@@ -164,7 +164,7 @@ public class DlgUser extends javax.swing.JDialog {
         tbUser.setPreferredScrollableViewportSize(new Dimension(500,500));
         tbUser.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
-        for (i = 0; i < 197; i++) {
+        for (i = 0; i < 198; i++) {
             TableColumn column = tbUser.getColumnModel().getColumn(i);
             if(i==0){
                 column.setPreferredWidth(170);
@@ -605,7 +605,7 @@ public class DlgUser extends javax.swing.JDialog {
                     "'false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false',"+
                     "'false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false',"+
                     "'false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false',"+
-                    "'false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false'","User")==true){
+                    "'false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false'","User")==true){
                 tampil();
                 emptTeks();
             }            
@@ -845,7 +845,8 @@ public class DlgUser extends javax.swing.JDialog {
                     "suplier_penunjang='"+tbUser.getValueAt(i,193).toString()+"',"+
                     "utd_donor='"+tbUser.getValueAt(i,194).toString()+"',"+
                     "bpjs_monitoring_klaim='"+tbUser.getValueAt(i,195).toString()+"',"+
-                    "utd_cekal_darah='"+tbUser.getValueAt(i,196).toString()+"'");
+                    "utd_cekal_darah='"+tbUser.getValueAt(i,196).toString()+"',"+
+                    "utd_komponen_darah='"+tbUser.getValueAt(i,197).toString()+"'");
             }            
             tampil();
             emptTeks();
@@ -1091,7 +1092,7 @@ private void BtnPrintKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_
                         "sirkulasi_obat2,edit_registrasi,bpjs_referensi_diagnosa,bpjs_referensi_poli,industrifarmasi,harian_js,bulanan_js,"+
                         "harian_paket_bhp,bulanan_paket_bhp,piutang_pasien2,bpjs_referensi_faskes,bpjs_sep,pengambilan_utd,tarif_utd, "+
                         "pengambilan_utd2,utd_medis_rusak,pengambilan_penunjang_utd,pengambilan_penunjang_utd2,utd_penunjang_rusak,"+
-                        "suplier_penunjang,utd_donor,bpjs_monitoring_klaim,utd_cekal_darah from user order by AES_DECRYPT(id_user,'nur')");
+                        "suplier_penunjang,utd_donor,bpjs_monitoring_klaim,utd_cekal_darah,utd_komponen_darah from user order by AES_DECRYPT(id_user,'nur')");
             try {
                 rs=ps.executeQuery();
                 while(rs.next()){
@@ -1298,7 +1299,8 @@ private void BtnPrintKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_
                                            rs.getBoolean("suplier_penunjang"),
                                            rs.getBoolean("utd_donor"),
                                            rs.getBoolean("bpjs_monitoring_klaim"),
-                                           rs.getBoolean("utd_cekal_darah")
+                                           rs.getBoolean("utd_cekal_darah"),
+                                           rs.getBoolean("utd_komponen_darah")
                             });
                         }   
                     } catch (Exception e) {
@@ -1497,7 +1499,8 @@ private void BtnPrintKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_
                                            rs.getBoolean("suplier_penunjang"),
                                            rs.getBoolean("utd_donor"),
                                            rs.getBoolean("bpjs_monitoring_klaim"),
-                                           rs.getBoolean("utd_cekal_darah")
+                                           rs.getBoolean("utd_cekal_darah"),
+                                           rs.getBoolean("utd_komponen_darah")
                             });
                     }                                             
                  }
