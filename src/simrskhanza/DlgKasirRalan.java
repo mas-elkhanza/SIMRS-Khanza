3e04233200aa6684901080bc46297b89bec62f21
khanzasoft <khanzasoft@khanzasoft>
2017-06-07 08:49:56 +0700

    Lihat lisensi.txt/README.txt bro. SUDAH FIX. Perbaikan data umur

--- src/simrskhanza/DlgKasirRalan.java
+++ src/simrskhanza/DlgKasirRalan.java
@@ -3318,6 +3318,7 @@ private void MnDataPemberianObatActionPerformed(java.awt.event.ActionEvent evt) 
             Sequel.queryu("delete from stok_obat_pasien where no_rawat='"+TNoRw.getText()+"'");
             Sequel.queryu("delete from detail_nota_jalan where no_rawat='"+TNoRw.getText()+"'");
             Sequel.queryu("delete from detail_piutang_pasien where no_rawat='"+TNoRw.getText()+"'");
+            Sequel.queryu("delete from mutasi_berkas where no_rawat='"+TNoRw.getText()+"'");
             Sequel.queryu("delete from reg_periksa where no_rawat='"+TNoRw.getText()+"'");
             Sequel.AutoComitTrue();
             tampilkasir();
