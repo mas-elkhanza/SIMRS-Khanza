1f6af2af3a498f2ed18927d7910c38b5c3e3e434
mas-elkhanza <khanza_media@yahoo.com>
2019-02-28 00:06:23 +0700

    Lihat lisensi.txt/README.txt bro. Sudah Fix. Perbaikan interface di mac

--- src/inventaris/InventarisKoleksi.java
+++ src/inventaris/InventarisKoleksi.java
@@ -301,7 +301,6 @@ public final class InventarisKoleksi extends javax.swing.JDialog {
 
         Popup.setName("Popup"); // NOI18N
 
-        ppBarcode.setBackground(new java.awt.Color(242, 242, 242));
         ppBarcode.setFont(new java.awt.Font("Tahoma", 0, 11)); // NOI18N
         ppBarcode.setForeground(new java.awt.Color(70, 70, 70));
         ppBarcode.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/b_print.png"))); // NOI18N
