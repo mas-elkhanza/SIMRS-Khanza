281ed765a88f965ed9cffff3f8b2c5a05ef25c67
mas-elkhanza <khanza_media@yahoo.com>
2019-05-17 18:45:50 +0700

    Lihat lisensi.txt/README.txt bro. Sudah Fix. Penyesuaian Antrian Loket dan Apotek mengikuti konfigurasi java 12

--- src/keuangan/DlgPiutangBelumLunas.java
+++ src/keuangan/DlgPiutangBelumLunas.java
@@ -18,7 +18,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import javax.swing.JOptionPane;
