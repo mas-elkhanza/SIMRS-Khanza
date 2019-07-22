d4fa0db5189ed5c7fbdb22b18cc8188b177f8551
mas-elkhanza <khanza_media@yahoo.com>
2019-07-22 19:56:19 +0700

    Lihat lisensi.txt/README.txt bro. Belum Fix Triase IGD. Sudah Fix Penambahan edit hasil nilai di pemeriksaan lab

--- src/simrskhanza/DlgCariPeriksaLab.java
+++ src/simrskhanza/DlgCariPeriksaLab.java
@@ -349,6 +349,7 @@ public class DlgCariPeriksaLab extends javax.swing.JDialog {
         MnCetakHasilLab21 = new javax.swing.JMenuItem();
         MnCetakNota = new javax.swing.JMenuItem();
         MnUbah = new javax.swing.JMenuItem();
+        MnUbah1 = new javax.swing.JMenuItem();
         MnSaranKesan = new javax.swing.JMenuItem();
         WindowSaran = new javax.swing.JDialog();
         internalFrame6 = new widget.InternalFrame();
@@ -757,6 +758,20 @@ public class DlgCariPeriksaLab extends javax.swing.JDialog {
             }
         });
         jPopupMenu1.add(MnUbah);
+
+        MnUbah1.setBackground(new java.awt.Color(255, 255, 254));
+        MnUbah1.setFont(new java.awt.Font("Tahoma", 0, 11)); // NOI18N
+        MnUbah1.setForeground(new java.awt.Color(70, 70, 70));
+        MnUbah1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/category.png"))); // NOI18N
+        MnUbah1.setText("Ubah Nilai Hasil");
+        MnUbah1.setName("MnUbah1"); // NOI18N
+        MnUbah1.setPreferredSize(new java.awt.Dimension(160, 26));
+        MnUbah1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MnUbah1ActionPerformed(evt);
+            }
+        });
+        jPopupMenu1.add(MnUbah1);
 
         MnSaranKesan.setBackground(new java.awt.Color(255, 255, 254));
         MnSaranKesan.setFont(new java.awt.Font("Tahoma", 0, 11)); // NOI18N
@@ -1796,6 +1811,7 @@ private void tbDokterKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_
                 TCari.requestFocus();
             }else{
                 DlgUbahPeriksaLab ubah=new DlgUbahPeriksaLab(null,false);
+                ubah.isCek();
                 ubah.setSize(this.getWidth()-20,this.getHeight()-20);
                 ubah.setNoRm(tbDokter.getValueAt(tbDokter.getSelectedRow(),0).toString(),
                         tbDokter.getValueAt(tbDokter.getSelectedRow(),3).toString(), 
@@ -4735,6 +4751,26 @@ private void tbDokterKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_
         this.setCursor(Cursor.getDefaultCursor());
     }//GEN-LAST:event_MnCetakHasilLab21ActionPerformed
 
+    private void MnUbah1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MnUbah1ActionPerformed
+        this.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+        if(tabMode.getRowCount()==0){
+            JOptionPane.showMessageDialog(null,"Maaf, data sudah habis...!!!!");
+            TCari.requestFocus();
+        }else if(Kd2.getText().trim().equals("")){
+            JOptionPane.showMessageDialog(null,"Maaf, Gagal mengubah. Pilih dulu data yang mau dicetak.\nKlik No.Rawat pada table untuk memilih...!!!!");
+        }else if(!(Kd2.getText().trim().equals(""))){  
+            DlgUbahNilaiLab ubah=new DlgUbahNilaiLab(null,false);
+            ubah.isCek();
+            ubah.setNoRm(tbDokter.getValueAt(tbDokter.getSelectedRow(),0).toString(),
+                    tbDokter.getValueAt(tbDokter.getSelectedRow(),3).toString(), 
+                    tbDokter.getValueAt(tbDokter.getSelectedRow(),4).toString());
+            ubah.setSize(this.getWidth()-20,this.getHeight()-20);
+            ubah.setLocationRelativeTo(this);
+            ubah.setVisible(true);
+        }
+        this.setCursor(Cursor.getDefaultCursor());
+    }//GEN-LAST:event_MnUbah1ActionPerformed
+
     /**
     * @param args the command line arguments
     */
@@ -4789,6 +4825,7 @@ private void tbDokterKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_
     private javax.swing.JMenu MnPDFLab;
     private javax.swing.JMenuItem MnSaranKesan;
     private javax.swing.JMenuItem MnUbah;
+    private javax.swing.JMenuItem MnUbah1;
     private widget.TextBox NoRawat;
     private widget.TextArea Saran;
     private widget.ScrollPane Scroll;
