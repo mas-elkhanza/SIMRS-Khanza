c3a6f4a783e662d86e1a46d502d8dc83fc9710e1
Mohammad Rizki <mohammadrizki.md@gmail.com>
2020-01-30 20:37:44 +0800

    Update rilis

--- src/fungsi/koneksiDB.java
+++ src/fungsi/koneksiDB.java
@@ -53,7 +53,7 @@ public class koneksiDB {
                         "                                                                           \n"+
                         "  Lisensi yang dianut di software ini https://en.wikipedia.org/wiki/Aladdin_Free_Public_License \n"+
                         "  Informasi dan panduan bisa dicek di halaman https://github.com/mas-elkhanza/SIMRS-Khanza/wiki \n"+
-                        "  Update 02/01/2020   ver. 1.0 - Kustom RS Unram                                                                   ");
+                        "  Update 30/01/2020   ver. 1.1-beta1 - Kustom RS Unram                                                                   ");
             }catch(Exception e){
                 JOptionPane.showMessageDialog(null,"Koneksi Putus : "+e);
             }
