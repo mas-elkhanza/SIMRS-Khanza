2368a2d365a759bc7410e1e6abed4dd0cc869718
yyaayyaatt <yyaayyaatt@gmail.com>
2020-11-17 07:48:04 +0700

    Update lagi dan lagi

--- src/simrskhanza/SIMRSKhanza.java
+++ src/simrskhanza/SIMRSKhanza.java
@@ -5,28 +5,6 @@
  */
 package simrskhanza;
 
-import com.jtattoo.plaf.acryl.AcrylLookAndFeel;
-import com.jtattoo.plaf.aero.AeroLookAndFeel;
-import com.jtattoo.plaf.bernstein.BernsteinLookAndFeel;
-import com.jtattoo.plaf.fast.FastLookAndFeel;
-import com.jtattoo.plaf.graphite.GraphiteLookAndFeel;
-import com.jtattoo.plaf.hifi.HiFiLookAndFeel;
-import com.jtattoo.plaf.luna.LunaLookAndFeel;
-import com.jtattoo.plaf.mcwin.McWinLookAndFeel;
-import com.jtattoo.plaf.noire.NoireLookAndFeel;
-import com.jtattoo.plaf.smart.SmartLookAndFeel;
-import com.jtattoo.plaf.texture.TextureLookAndFeel;
-import com.sun.java.swing.plaf.gtk.GTKLookAndFeel;
-import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.plaf.basic.BasicLookAndFeel;
-import javax.swing.plaf.metal.MetalLookAndFeel;
-import javax.swing.plaf.multi.MultiLookAndFeel;
-import javax.swing.plaf.nimbus.NimbusLookAndFeel;
-import javax.swing.plaf.synth.SynthLookAndFeel;
 import usu.widget.util.WidgetUtilities;
 
 /**
@@ -39,17 +17,12 @@ public class SIMRSKhanza {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        try {
-            System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
-//            UIManager.setLookAndFeel(new WindowsLookAndFeel());
-            WidgetUtilities.invokeLater(() -> {
+        System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
+        WidgetUtilities.invokeLater(() -> {
 
-                frmUtama utama = frmUtama.getInstance();
-                utama.isWall();
-                utama.setVisible(true);
-            });
-//        } catch (UnsupportedLookAndFeelException ex) {
-//            Logger.getLogger(SIMRSKhanza.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+            frmUtama utama = frmUtama.getInstance();
+            utama.isWall();
+            utama.setVisible(true);
+        });
     }
 }
