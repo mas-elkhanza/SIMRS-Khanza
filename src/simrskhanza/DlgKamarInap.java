678dabb84afe6bec540bb7ca2383bd973596a3e8
Fredy Nur Apriyanto <mail.fredyna@gmail.com>
2021-03-30 14:02:58 +0700

    Revert "Dev shin"

--- src/simrskhanza/DlgKamarInap.java
+++ src/simrskhanza/DlgKamarInap.java
@@ -12355,10 +12355,6 @@ public class DlgKamarInap extends javax.swing.JDialog {
      * @param norm
      * @param nmpasien
      */
-    public void setDiagnosaMasuk(String diagnosa){
-        diagnosaawal.setText(diagnosa);
-    }
-    
     public void setNoRm(String norwt, String norm, String nmpasien) {
         norawat.setText(norwt);
         TNoRM.setText(norm);
