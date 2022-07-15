1f9d5b1f630b66d3e4d15019f45e2d0a9b7e0752
mas-elkhanza <khanza_media@yahoo.com>
2022-07-15 15:49:56 +0700

    Lihat lisensi.txt/README.txt bro. Bridging Apotek BPJS Belum Fix. Bridging BRI API BRIVA Belum Fix. Belum Fix Persetujuan/Penolakan Tindakan

--- src/simrskhanza/frmUtama.java
+++ src/simrskhanza/frmUtama.java
@@ -1760,7 +1760,7 @@ public class frmUtama extends javax.swing.JFrame {
 
         tanggal.setEditable(false);
         tanggal.setForeground(new java.awt.Color(50, 70, 50));
-        tanggal.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "14/07/2022" }));
+        tanggal.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "15/07/2022" }));
         tanggal.setDisplayFormat("dd/MM/yyyy");
         tanggal.setName("tanggal"); // NOI18N
         tanggal.setOpaque(false);
@@ -6831,7 +6831,7 @@ public class frmUtama extends javax.swing.JFrame {
         internalFrame1.setPreferredSize(new java.awt.Dimension(40, 42));
         internalFrame1.setVerifyInputWhenFocusTarget(false);
         internalFrame1.setWarnaAtas(new java.awt.Color(255, 250, 250));
-        internalFrame1.setWarnaBawah(new java.awt.Color(255, 210, 210));
+        internalFrame1.setWarnaBawah(new java.awt.Color(255, 205, 205));
         internalFrame1.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT, 0, 2));
 
         BtnMenu.setBackground(new java.awt.Color(255, 255, 255));
@@ -7111,7 +7111,7 @@ public class frmUtama extends javax.swing.JFrame {
         internalFrame4.setBorder(null);
         internalFrame4.setName("internalFrame4"); // NOI18N
         internalFrame4.setPreferredSize(new java.awt.Dimension(330, 25));
-        internalFrame4.setWarnaAtas(new java.awt.Color(255, 215, 215));
+        internalFrame4.setWarnaAtas(new java.awt.Color(255, 210, 210));
         internalFrame4.setWarnaBawah(new java.awt.Color(255, 250, 250));
         internalFrame4.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT, 4, 1));
 
