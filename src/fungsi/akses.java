38735e35ee851b4fba1fc22371f36a6250c6b21c
mas-elkhanza <khanza_media@yahoo.com>
2022-07-30 23:37:40 +0700

    awal medis penyakit dalam

--- src/fungsi/akses.java
+++ src/fungsi/akses.java
@@ -186,7 +186,7 @@ public final class akses {
             audit_kamar_jenazah=false,audit_bundle_iadp=false,audit_bundle_ido=false,audit_fasilitas_kebersihan_tangan=false,audit_fasilitas_apd=false,
             audit_pembuangan_limbah_cair_infeksius=false,audit_sterilisasi_alat=false,penilaian_awal_medis_ralan_psikiatri=false,persetujuan_penolakan_tindakan=false,
             audit_bundle_isk=false,audit_bundle_plabsi=false,audit_bundle_vap=false,akun_host_to_host_bank_papua=false,pembayaran_bank_papua=false,
-            penilaian_awal_medis_ralan_penyakit_dalam=false;
+            penilaian_awal_medis_ralan_penyakit_dalam=false,penilaian_awal_medis_ralan_mata=false;
     
     public static void setData(String user, String pass) {
        try {                
@@ -1058,6 +1058,7 @@ public final class akses {
                         akses.akun_host_to_host_bank_papua=true;
                         akses.pembayaran_bank_papua=true;
                         akses.penilaian_awal_medis_ralan_penyakit_dalam=true;
+                        akses.penilaian_awal_medis_ralan_mata=true;
                     }else if(rs2.getRow()>=1){   
                         rs2.beforeFirst();
                         rs2.next();
@@ -1913,6 +1914,7 @@ public final class akses {
                         akses.akun_host_to_host_bank_papua=rs2.getBoolean("akun_host_to_host_bank_papua");
                         akses.pembayaran_bank_papua=rs2.getBoolean("pembayaran_bank_papua");
                         akses.penilaian_awal_medis_ralan_penyakit_dalam=rs2.getBoolean("penilaian_awal_medis_ralan_penyakit_dalam");
+                        akses.penilaian_awal_medis_ralan_mata=rs2.getBoolean("penilaian_awal_medis_ralan_mata");
                     }else if((rs.getRow()==0)&&(rs2.getRow()==0)){
                         akses.kode="";                  
                         akses.penyakit= false;
@@ -2766,6 +2768,7 @@ public final class akses {
                         akses.akun_host_to_host_bank_papua=false;
                         akses.pembayaran_bank_papua=false;
                         akses.penilaian_awal_medis_ralan_penyakit_dalam=false;
+                        akses.penilaian_awal_medis_ralan_mata=false;
                     }
                 } catch (Exception e) {
                     System.out.println("Notifikasi : "+e);
@@ -3676,4 +3679,5 @@ public final class akses {
     public static boolean getakun_host_to_host_bank_papua(){return akses.akun_host_to_host_bank_papua;}
     public static boolean getpembayaran_bank_papua(){return akses.pembayaran_bank_papua;}
     public static boolean getpenilaian_awal_medis_ralan_penyakit_dalam(){return akses.penilaian_awal_medis_ralan_penyakit_dalam;}
+    public static boolean getpenilaian_awal_medis_ralan_mata(){return akses.penilaian_awal_medis_ralan_mata;}
 }   
--- src/setting/DlgUser.java
+++ src/setting/DlgUser.java
@@ -209,7 +209,7 @@ public class DlgUser extends javax.swing.JDialog {
                 "[C]Audit Penanganan Darah","[C]Audit Pengelolaan Linen Kotor","[C]Audit Penempatan Pasien","[C]Audit Kamar Jenazah","[C]Audit Bundle IADP",
                 "[C]Audit Bundle IDO","[C]Audit Fasilitas Kebersihan Tangan","[C]Audit Fasilitas APD","[C]Audit Pembuangan Limbah Cair Infeksius","[C]Audit Sterilisasi Alat",
                 "[L]Penilaian Awal Medis Ralan Psikiatri","[O]Persetujuan/Penolakan Tindakan","[C]Audit Bundle ISK","[C]Audit Bundle PLABSI","[C]Audit Bundle VAP",
-                "[K]Host To Host Bank Papua","[J]Pembayaran Bank Papua","[L]Penilaian Awal Medis Ralan Penyakit Dalam"
+                "[K]Host To Host Bank Papua","[J]Pembayaran Bank Papua","[L]Penilaian Awal Medis Ralan Penyakit Dalam","[L]Penilaian Awal Medis Ralan Mata"
         };
         
         tabMode=new DefaultTableModel(null,row){
@@ -457,7 +457,7 @@ public class DlgUser extends javax.swing.JDialog {
         tbUser.setPreferredScrollableViewportSize(new Dimension(500,500));
         tbUser.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
-        for (i = 0; i < 852;i++) {
+        for (i = 0; i < 853;i++) {
             TableColumn column = tbUser.getColumnModel().getColumn(i);
             switch (i) {
                 case 0:
@@ -2467,6 +2467,9 @@ public class DlgUser extends javax.swing.JDialog {
                 case 851:
                     column.setPreferredWidth(233);
                     break;
+                case 852:
+                    column.setPreferredWidth(181);
+                    break;
                 default:
                     column.setPreferredWidth(135);
                     break;
@@ -2967,7 +2970,7 @@ public class DlgUser extends javax.swing.JDialog {
                     "'false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false',"+
                     "'false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false',"+
                     "'false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false',"+
-                    "'false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false'","User")==true){
+                    "'false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false','false'","User")==true){
                 tampil();
                 emptTeks();
             }            
@@ -3861,7 +3864,8 @@ public class DlgUser extends javax.swing.JDialog {
                     "audit_bundle_vap='"+tbUser.getValueAt(i,848).toString()+"',"+
                     "akun_host_to_host_bank_papua='"+tbUser.getValueAt(i,849).toString()+"',"+
                     "pembayaran_bank_papua='"+tbUser.getValueAt(i,850).toString()+"',"+
-                    "penilaian_awal_medis_ralan_penyakit_dalam='"+tbUser.getValueAt(i,851).toString()+"'");
+                    "penilaian_awal_medis_ralan_penyakit_dalam='"+tbUser.getValueAt(i,851).toString()+"',"+
+                    "penilaian_awal_medis_ralan_mata='"+tbUser.getValueAt(i,852).toString()+"'");
             }            
             tampil();
             emptTeks();
@@ -4795,7 +4799,8 @@ public class DlgUser extends javax.swing.JDialog {
                                         "audit_bundle_vap='"+tbUser.getValueAt(barisdicopy,848).toString()+"',"+
                                         "akun_host_to_host_bank_papua='"+tbUser.getValueAt(barisdicopy,849).toString()+"',"+
                                         "pembayaran_bank_papua='"+tbUser.getValueAt(barisdicopy,850).toString()+"',"+
-                                        "penilaian_awal_medis_ralan_penyakit_dalam='"+tbUser.getValueAt(barisdicopy,851).toString()+"'");
+                                        "penilaian_awal_medis_ralan_penyakit_dalam='"+tbUser.getValueAt(barisdicopy,851).toString()+"',"+
+                                        "penilaian_awal_medis_ralan_mata='"+tbUser.getValueAt(barisdicopy,852).toString()+"'");
                                 }    
                                 userdicopy="";
                                 copyhakakses="";
@@ -5164,7 +5169,7 @@ private void BtnPrintKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_
                 "user.audit_pengelolaan_linen_kotor,user.audit_penempatan_pasien,user.audit_kamar_jenazah,user.audit_bundle_iadp,user.audit_bundle_ido,"+
                 "user.audit_fasilitas_kebersihan_tangan,user.audit_fasilitas_apd,user.audit_pembuangan_limbah_cair_infeksius,user.audit_sterilisasi_alat,"+
                 "user.penilaian_awal_medis_ralan_psikiatri,user.persetujuan_penolakan_tindakan,user.audit_bundle_isk,user.audit_bundle_plabsi,user.audit_bundle_vap,"+
-                "user.akun_host_to_host_bank_papua,user.pembayaran_bank_papua,user.penilaian_awal_medis_ralan_penyakit_dalam from user order by AES_DECRYPT(user.id_user,'nur')");
+                "user.akun_host_to_host_bank_papua,user.pembayaran_bank_papua,user.penilaian_awal_medis_ralan_penyakit_dalam,user.penilaian_awal_medis_ralan_mata from user order by AES_DECRYPT(user.id_user,'nur')");
             try {
                 rs=ps.executeQuery();
                 while(rs.next()){
@@ -6028,7 +6033,8 @@ private void BtnPrintKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_
                                rs.getBoolean("audit_bundle_vap"),
                                rs.getBoolean("akun_host_to_host_bank_papua"),
                                rs.getBoolean("pembayaran_bank_papua"),
-                               rs.getBoolean("penilaian_awal_medis_ralan_penyakit_dalam")
+                               rs.getBoolean("penilaian_awal_medis_ralan_penyakit_dalam"),
+                               rs.getBoolean("penilaian_awal_medis_ralan_mata")
                             });
                         }   
                     } catch (Exception e) {
@@ -6881,7 +6887,8 @@ private void BtnPrintKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_
                            rs.getBoolean("audit_bundle_vap"),
                            rs.getBoolean("akun_host_to_host_bank_papua"),
                            rs.getBoolean("pembayaran_bank_papua"),
-                           rs.getBoolean("penilaian_awal_medis_ralan_penyakit_dalam")
+                           rs.getBoolean("penilaian_awal_medis_ralan_penyakit_dalam"),
+                           rs.getBoolean("penilaian_awal_medis_ralan_mata")
                         });
                     }                                             
                  }
