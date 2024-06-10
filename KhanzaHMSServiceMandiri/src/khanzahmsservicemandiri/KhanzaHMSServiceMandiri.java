07b08fc7b0a04428aed9513e3447b89ff0ccde19
mas-elkhanza <khanza_media@yahoo.com>
2024-06-10 08:17:05 +0700

    Belum Fix Obat 23 Hari Apotek BPJS, Belum Fix Integrasi Pembayaran Pihak Ke 3 MCM Bank Mandiri

---
+++ KhanzaHMSServiceMandiri/src/khanzahmsservicemandiri/KhanzaHMSServiceMandiri.java
@@ -0,0 +1,17 @@
+
+package khanzahmsservicemandiri;
+
+/**
+ *
+ * @author windiartonugroho
+ */
+public class KhanzaHMSServiceMandiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frmUtama().setVisible(true);
+    }
+    
+}
