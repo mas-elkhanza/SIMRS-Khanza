4c608ca24667fdee408d41e7e5fbb1bf3805f5ab
mas-elkhanza <khanza_media@yahoo.com>
2025-01-08 07:22:23 +0700

    Belum Fix Obat 23 Hari Apotek BPJS, Penambahan menu logistik dapur

--- src/rekammedis/RMPenilaianAwalMedisRanapNeonatus.java
+++ src/rekammedis/RMPenilaianAwalMedisRanapNeonatus.java
@@ -98,9 +98,29 @@ public final class RMPenilaianAwalMedisRanapNeonatus extends javax.swing.JDialog
             }else if(i==3){
                 column.setPreferredWidth(65);
             }else if(i==4){
-                column.setPreferredWidth(55);
+                column.setPreferredWidth(60);
             }else if(i==5){
-                column.setPreferredWidth(80);
+                column.setPreferredWidth(40);
+            }else if(i==6){
+                column.setPreferredWidth(110);
+            }else if(i==7){
+                column.setPreferredWidth(70);
+            }else if(i==8){
+                column.setPreferredWidth(150);
+            }else if(i==9){
+                column.setPreferredWidth(110);
+            }else if(i==10){
+                column.setPreferredWidth(90);
+            }else if(i==11){
+                column.setPreferredWidth(150);
+            }else if(i==12){
+                column.setPreferredWidth(150);
+            }else if(i==13){
+                column.setPreferredWidth(30);
+            }else if(i==14){
+                column.setPreferredWidth(30);
+            }else if(i==15){
+                column.setPreferredWidth(30);
             }
         }
         tbObat.setDefaultRenderer(Object.class, new WarnaTable());
