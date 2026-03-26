db24282765570c8db7634047e8264948280cee95
tejasae-afk <tejasae-afk@users.noreply.github.com>
2026-03-25 22:06:07 -0400

    Close FileInputStream in play

--- KhanzaAntrianApotek/src/fungsi/BackgroundMusic.java
+++ KhanzaAntrianApotek/src/fungsi/BackgroundMusic.java
@@ -29,12 +29,13 @@ public class BackgroundMusic {
     // play the MP3 file to the sound card
     public void play() {
         try {
-            FileInputStream fis = new FileInputStream(filename);
-            BufferedInputStream bis = new BufferedInputStream(fis);
-            player = new Player(bis);
-        } catch (Exception e) {
-            System.out.println("Problem playing file " + filename);
-            System.out.println(e);
+            try (FileInputStream fis = new FileInputStream(filename)) {
+                BufferedInputStream bis = new BufferedInputStream(fis);
+                player = new Player(bis);
+                } catch (Exception e) {
+                System.out.println("Problem playing file " + filename);
+                System.out.println(e);
+            }
         }
     }
 
