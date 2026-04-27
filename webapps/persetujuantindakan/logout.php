ef69ccade409ad39b4e01194a7531406a2c93e5e
itrspelitakasih <itrspelitakasih@gmail.com+itrspelitakasih@users.noreply.github.com>
2026-04-27 08:14:39 +0800

    Apply gitignore rules

--- webapps/persetujuantindakan/logout.php
+++
@@ -1,9 +0,0 @@
-<?php	
-    session_start();
-    session_destroy();
-    require_once "conf/command.php";
-    if (cekSessiAdmin()){
-        session_unregister("ses_admin_persetujuantindakan");
-    }
-    header("Location:index.php");
-?>
