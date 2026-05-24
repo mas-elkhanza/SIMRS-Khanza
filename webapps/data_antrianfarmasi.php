3e4f4e6b6c898c736b8e5f47d8dd6635fcbe6051
mas-elkhanza <khanza_media@yahoo.com>
2026-05-24 20:58:16 +0700

    Optimasi query & memory

--- webapps/data_antrianfarmasi.php
+++ webapps/data_antrianfarmasi.php
@@ -1,13 +1,13 @@
  <?php
- require_once('conf/conf.php');
- header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
- header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT"); 
- header("Cache-Control: no-store, no-cache, must-revalidate"); 
- header("Cache-Control: post-check=0, pre-check=0", false);
- header("Pragma: no-cache"); // HTTP/1.0
- date_default_timezone_set("Asia/Bangkok");
- $tanggal= mktime(date("m"),date("d"),date("Y"));
- $jam=date("H:i");
+    require_once('conf/conf.php');
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
+    header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT"); 
+    header("Cache-Control: no-store, no-cache, must-revalidate"); 
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache"); // HTTP/1.0
+    date_default_timezone_set("Asia/Bangkok");
+    $tanggal= mktime(date("m"),date("d"),date("Y"));
+    $jam=date("H:i");
 ?>
  <div class="col s12 row">
     <div class="col s12">
